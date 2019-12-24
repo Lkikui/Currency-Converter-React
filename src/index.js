9d41d034ae707cd2f64f4890884080f7791e1458
LisaRyland <lisa.ryland@gmail.com>
2019-12-24 14:52:58 -0800

    built template and dynamic dropdown

--- src/index.js
+++ src/index.js
@@ -1,8 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { get } from 'axios';
+
+class App extends React.Component {
+    state = {
+        exchangeRates: [],
+        foreignCurrencies: [],
+        selectedCurrency: '',
+    }
+
+    componentDidMount() {
+        // api: https://api.exchangeratesapi.io/latest?base=USD
+        get('https://api.exchangeratesapi.io/latest?base=USD').then(response => {
+            // destructure, sets as empty obj. when no data
+            const { data: { rates: currencies } = {} } = response;
+            
+            // currency object (including currency and their rates)
+            this.setState({ exchangeRates: currencies });
+
+            // currencies
+            const foreignCurrencies = Object.keys(currencies);
+            this.setState({ foreignCurrencies });
+        });
+    }
+
+    render() {
+        console.log(this.state);
+
+        const exchangeTo = this.state.foreignCurrencies.map((currency, key) => (
+            <option key={currency} value={currency}>
+                {currency}{' '}
+            </option>
+        ));
+
+        return(
+            <div className="App">
+                <h1>Exchange Rates</h1>
+                <br />
+
+                <div className="baseCurrency">
+                    <h2>Base Currency</h2>
+                    USD:
+                    <input type="text" name="baseAmount" id=""/>
+                    <br />
+                    Exchange to:
+                    <select name="" id="">
+                        {exchangeTo}
+                    </select>
+                    <button>Convert!</button>
+                </div>
+
+                <div className="convertedCurrency">
+                    <h2>Converted Currency</h2>
+                </div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
